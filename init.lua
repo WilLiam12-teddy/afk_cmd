f219e1f955c9676733c311a23a82a1cfea3c8b2b
WilLiam12-teddy <willteddydoze@gmail.com>
2020-06-19 13:34:41 -0300

    .

---
+++ init.lua
@@ -0,0 +1,9 @@
+minetest.register_chatcommand("afk", {
+    description = "Tell everyone you are afk.",
+	privs = {interact=true},
+    func = function ( name, param )
+        local player = minetest.get_player_by_name(name)
+        minetest.chat_send_all(name.." is AFK! "..param)
+        return true
+    end,
+})
